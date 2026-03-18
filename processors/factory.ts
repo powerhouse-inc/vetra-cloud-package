0719f505331968424d5227b827aeda5832b621e5
Frank <frank.pfeift@protonmail.com>
2026-03-18 13:49:26 +0100

    refactor: remove debug logging from processor factory

--- processors/factory.ts
+++ processors/factory.ts
@@ -6,15 +6,5 @@ import type { PHDocumentHeader } from "document-model";
 import { vetraCloudEnvironmentProcessorFactory } from "./vetra-cloud-environment/factory.js";
 
 export const processorFactory = (module: IProcessorHostModule) => {
-  console.log("[vetra-cloud-environment] processorFactory called, module keys:", Object.keys(module));
-  const factory = vetraCloudEnvironmentProcessorFactory(module);
-
-  const wrappedFactory = async (driveHeader: PHDocumentHeader): Promise<ProcessorRecord[]> => {
-    console.log("[vetra-cloud-environment] wrappedFactory called for drive:", driveHeader.id);
-    const result = await factory(driveHeader);
-    console.log("[vetra-cloud-environment] wrappedFactory returning", result.length, "processor records");
-    return result;
-  };
-
-  return wrappedFactory;
+  return vetraCloudEnvironmentProcessorFactory(module);
 };
